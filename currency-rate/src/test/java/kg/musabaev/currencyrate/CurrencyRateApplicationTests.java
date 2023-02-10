679632a3d33c2c990b00ab9790091844f9fba938
Eldiyar Musabaev <musabaeveldiar@gmail.com>
2023-02-10 19:35:11 +0600

    add  tests

--- currency-rate/src/test/java/kg/musabaev/currencyrate/CurrencyRateApplicationTests.java
+++ currency-rate/src/test/java/kg/musabaev/currencyrate/CurrencyRateApplicationTests.java
@@ -1,13 +1,52 @@
 package kg.musabaev.currencyrate;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.server.ResponseStatusException;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 class CurrencyRateApplicationTests {
 
+	@InjectMocks
+	CbrController cbrController;
+
+	@Mock
+	CbrCurrencyRateFeignClient currencyRateFeignClient;
+
 	@Test
-	void contextLoads() {
+	void shouldFindRate() {
+		var currencyRates = getCbrCurrencyRateResponse();
+
+		when(currencyRateFeignClient.getAllCbrCurrencyRate()).thenReturn(currencyRates);
+
+		CurrencyRate kgsRate = cbrController.getCurrencyRate("KGS");
+
+		assertThat(kgsRate).isNotNull();
+		assertThat(kgsRate.getValue()).isEqualTo(1.124);
+	}
+
+	@Test
+	void shouldNotFindRate() {
+		var currencyRates = getCbrCurrencyRateResponse();
+
+		when(currencyRateFeignClient.getAllCbrCurrencyRate()).thenReturn(currencyRates);
+
+		assertThatExceptionOfType(ResponseStatusException.class).isThrownBy(() ->
+				cbrController.getCurrencyRate("KZT"));
+	}
+
+	private CbrCurrencyRateResponse getCbrCurrencyRateResponse() {
+		return CbrCurrencyRateResponse.builder()
+				.currencyRate(CurrencyRate.builder().charCode("KGS").value(1.124).build())
+				.currencyRate(CurrencyRate.builder().charCode("EUR").value(83.421).build())
+				.build();
 	}
 
 }
