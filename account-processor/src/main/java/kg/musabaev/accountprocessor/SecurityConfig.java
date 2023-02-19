f58b1b3c7aa6f7b480c5b38721d5e148577fab1c
Eldiyar Musabaev <musabaeveldiar@gmail.com>
2023-02-19 20:15:38 +0600

    configure security of account processor

--- account-processor/src/main/java/kg/musabaev/accountprocessor/SecurityConfig.java
+++ account-processor/src/main/java/kg/musabaev/accountprocessor/SecurityConfig.java
@@ -1,24 +1,30 @@
 package kg.musabaev.accountprocessor;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+	@Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+	private String issuerUri;
+
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		return http
-				.authorizeHttpRequests()
-				.requestMatchers("/**").hasAnyAuthority("SCOPE_account.read")
-				.and()
-				.oauth2ResourceServer()
-				.jwt()
-				.and()
+				.authorizeHttpRequests(matchers -> matchers
+						.requestMatchers(HttpMethod.POST, "/**").hasAuthority("SCOPE_account.write")
+						.requestMatchers(HttpMethod.PUT, "/**").hasAuthority("SCOPE_account.write")
+						.requestMatchers(HttpMethod.GET, "/**").hasAuthority("SCOPE_account.read"))
+				.oauth2ResourceServer().jwt(jwtConfigurer -> jwtConfigurer
+						.decoder(token -> JwtDecoders.fromIssuerLocation(issuerUri)))
 				.and()
 				.build();
 	}
